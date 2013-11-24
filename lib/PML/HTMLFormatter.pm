4c330ee13162c51be14bf45323c0b31f77cfa7ab
necrophonic <john@johngregory.me.uk>
2013-11-24 22:52:12 +0000

    Refactors html generators into overridable subs

--- lib/PML/HTMLFormatter.pm
+++ lib/PML/HTMLFormatter.pm
@@ -6,8 +6,8 @@ use strict;
 use warnings;
 use boolean;
 
-#use Log::Log4perl qw(:easy);
-#Log::Log4perl->easy_init($TRACE);
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($ERROR);
 
 use Moo;
 use PML::Tokenizer;
@@ -19,9 +19,10 @@ has 'is_column_open'    => ( is => 'rwp' );
 has 'is_row_open' 	    => ( is => 'rwp' );
 has 'stack'			    => ( is => 'rwp' );
 
-
 has 'html_chunks'	     => ( is => 'rwp' );
 has 'html_chunk_pointer' => ( is => 'rwp' );
+
+my $END = 1;
 
 # ------------------------------------------------------------------------------
 
@@ -43,12 +44,8 @@ sub format {
 
 	my $tmp_column_count = 0;
 
-	# TODO html target??
-
-
-
-	#TRACE "-----------------------------------------";
-	#TRACE "BEGIN HTML FORMAT";
+	TRACE "-----------------------------------------";
+	TRACE "BEGIN HTML FORMAT";
 	
 	my $tmp_row_html = '';
 	my %tmp_row_data = ();
@@ -57,7 +54,7 @@ sub format {
 		
 		my $type = $token->{type};
 
-		#TRACE "[Type: $type]";
+		TRACE "[Type: $type]";
 
 		if ($type eq 'STRING') {
 			$self->_open_paragraph_if_not_open;
@@ -83,25 +80,25 @@ sub format {
 					$self->fatal("Expecting to close paragraph but bad stack head");
 				}
 				$self->_pop_from_stack;				
-				$self->_append_to_html('</p>');
+				$self->_append_to_html( $self->_html_for_paragraph($END) );
 				$self->_set_is_paragraph_open(false);
 			}
 			else {
-				$self->_append_to_html('<p>');
+				$self->_append_to_html( $self->_html_for_paragraph );
 				$self->_set_is_paragraph_open(true);
 			}
 		}
 
 		elsif ($type eq 'LINE_BREAK') {
 			$self->_open_paragraph_if_not_open;			
-			$self->_append_to_html('<br>');
+			$self->_append_to_html( $self->_html_for_line_break );
 		}
 
 		# HEADER
 		elsif ($type eq 'HEADER') {			
 			my $level = $token->{level};
-			my $text  = $token->{text};
-			$self->_append_to_html("<h$level>$text</h$level>");
+			my $text  = $token->{text};			
+			$self->_append_to_html( $self->_html_for_header( $level, $text ));
 		}
 
 		# ROW
@@ -111,21 +108,19 @@ sub format {
 				# If a column is still open, then close it
 				if ($self->is_column_open) {					
 					$self->_close_paragraph_if_open;
-					$self->_append_to_html('</div>');
+					$self->_append_to_html( $self->_html_for_close_row );
 					$self->_set_is_column_open(false);	
 				}
-
 
 				# If there's already a row open, then this must close it.
 				$self->_set_is_row_open(false);
 				$self->_close_paragraph_if_open;
 
-				$self->html_chunks->[ $self->html_chunk_pointer ] =
-							'<div class="clearfix col-'.$tmp_column_count.'">'
-							. $self->html_chunks->[ $self->html_chunk_pointer ]
-							. '</div>';
-
-			
+				$self->html_chunks->[ $self->html_chunk_pointer ]
+							= $self->_html_for_open_row(
+										$tmp_column_count,
+										$self->html_chunks->[ $self->html_chunk_pointer ]
+							);
 
 				$tmp_column_count = 0; # Reset count
 			}
@@ -142,19 +137,18 @@ sub format {
 		# COLUMN
 		elsif ($type eq 'COLUMN') {			
 
-			unless ($self->is_row_open) {
-				$self->fatal("Column outside of row");
-			}
+			$self->fatal("Column outside of row") unless $self->is_row_open;
+			
 			if ($self->is_column_open) {
-				#TRACE "Column is open - closing";
+				TRACE "Column is open - closing";
 				$self->_close_paragraph_if_open;
-				$self->_append_to_html('</div>');				
+				$self->_append_to_html( $self->_html_for_close_column );				
 			}
 			
 			# If a previous paragraph was open, then we want to
 			# close it before we start a new column.
-			$tmp_column_count++;
-			$self->_append_to_html('<div class="column">');
+			$tmp_column_count++;			
+			$self->_append_to_html( $self->_html_for_open_column );
 			$self->_set_is_column_open(true);
 		}
 
@@ -163,40 +157,28 @@ sub format {
 			# If the image is inline (no specified alignment) then
 			# open a paragraph if one isn't open.			
 			$self->_open_paragraph_if_not_open unless $token->{align};
-			
-			my $align = '';
-			if ($token->{align}) {
-				if ($token->{align} eq 'left')  {$align = ' class="pulled-left"'     }
-				if ($token->{align} eq 'right') {$align = ' class="pulled-right"'    }
-				if ($token->{align} eq 'center'){$align = ' class="centered"' 	   }
-				if ($token->{align} eq 'span')  {$align = ' class="stretch-horiz"' }
-			}
-
-			$self->_append_to_html(
-					'<img src="'.$token->{src}.'"'
-					.$align
-					.($token->{width}  ? ' width="' .$token->{width} .'px"' : '')
-					.($token->{height} ? ' height="'.$token->{height}.'px"' : '')
-					.'>');
+			$self->_append_to_html( $self->_html_for_image(
+										$token->{src},
+										{
+											align  => $token->{align},
+											width  => $token->{width},
+											height => $token->{height},
+											align  => $token->{align},
+										}  
+			 						));
 		}
 
 		# LINK
 		elsif ($type eq 'LINK') {
 			$self->_open_paragraph_if_not_open;
-			$self->_append_to_html('<a href="'.$token->{href}.'"'
-				  .($token->{href}=~/^http/?' target="_new"':'')  # External links
-				  .'>'
-				  .($token->{text}?$token->{text}:$token->{href})
-				  .'</a>');
+			$self->_append_to_html( $self->_html_for_link( $token->{href}, $token->{text}) );
 		}
-
-
 	}
 
 	# Clean up
 	# Clear down stack
 	for my $tag (@{$self->stack}) {
-		if ($tag eq 'PARA') { $self->_append_to_html('</p>') }
+		if ($tag eq 'PARA') { $self->_append_to_html( $self->_html_for_paragraph($END)) }
 	}
 
 	return join '', @{$self->html_chunks};
@@ -206,9 +188,8 @@ sub format {
 
 sub _append_to_html {
 	my ($self, $text_to_append) = @_;	
-	#TRACE "Append '$text_to_append' to html chunk '".$self->html_chunk_pointer."'";
-	$self->html_chunks->[$self->html_chunk_pointer] .= $text_to_append;
-	#$self->_set_html( $self->html_buffer . $text_to_append );
+	TRACE "Append '$text_to_append' to html chunk '".$self->html_chunk_pointer."'";
+	$self->html_chunks->[$self->html_chunk_pointer] .= $text_to_append;	
 }
 
 # ------------------------------------------------------------------------------
@@ -231,7 +212,7 @@ sub _open_paragraph_if_not_open {
 		$self->_append_to_html('<p>');
 		$self->_set_is_paragraph_open(true);
 		$self->_add_to_stack('PARA');
-		#TRACE "Close paragaph";
+		TRACE "Close paragaph";
 	}
 }
 
@@ -252,11 +233,14 @@ sub _close_paragraph_if_open {
 # ------------------------------------------------------------------------------
 
 sub _output_simple {
-	my ($self, $tag, $html_tag) = @_;
+	my ($self, $tag) = @_;
 	my $match = $self->_is_tag_matched($tag);
 	if ($match) {
 		shift @{$self->stack}; # Pull match off stack
-		$self->_append_to_html('</'.$html_tag.'>');
+		$self->_append_to_html( $self->_html_for_strong($END) ) 	if $tag eq 'STRONG';
+		$self->_append_to_html( $self->_html_for_underline($END) ) 	if $tag eq 'UNDERLINE';
+		$self->_append_to_html( $self->_html_for_emphasis($END) ) 	if $tag eq 'EMPHASIS';
+		$self->_append_to_html( $self->_html_for_quote($END) ) 		if $tag eq 'QUOTE';
 	}
 	else {
 		# Check whether paragaph is open and open if not.
@@ -264,17 +248,19 @@ sub _output_simple {
 			$self->_append_to_html('<p>');
 			unshift @{$self->stack}, 'PARA';
 			$self->_set_is_paragraph_open(true);
-		}
-		$self->_append_to_html('<'.$html_tag.'>');
+		}		
+		$self->_append_to_html( $self->_html_for_strong ) 	 if $tag eq 'STRONG';
+		$self->_append_to_html( $self->_html_for_underline ) if $tag eq 'UNDERLINE';
+		$self->_append_to_html( $self->_html_for_emphasis )  if $tag eq 'EMPHASIS';
+		$self->_append_to_html( $self->_html_for_quote )     if $tag eq 'QUOTE';
 		unshift @{$self->stack}, $tag;
 	}
 }
 
 # ------------------------------------------------------------------------------
-
 sub _add_to_stack {
 	my ($self, $tag) = @_;
-	#TRACE "Add '$tag' to stack";
+	TRACE "Add '$tag' to stack";
 	unshift @{$self->stack}, $tag;
 }
 
@@ -283,7 +269,7 @@ sub _add_to_stack {
 sub _pop_from_stack {
 	my ($self, $stack_ref) = @_;
 	my $tag = shift @{$self->stack};
-	#TRACE "Pop '$tag' from stack";
+	TRACE "Pop '$tag' from stack";
 	return $tag;
 }
 
@@ -299,7 +285,7 @@ sub _is_tag_matched {
 sub fatal {
 	my ($self, @msg) = @_;
 	my $msg = join '',@msg;
-	#ERROR "!!HTMLFormatter Error: $msg!!";
+	ERROR "!!HTMLFormatter Error: $msg!!";
 	die "$msg\n\n";
 }
 
@@ -310,6 +296,117 @@ sub _build_tokenizer {
 	return PML::Tokenizer->new;
 }
 
+# --------------------------------------------------------[ HTML OUTPUTERS ]----
+
+# <sub>(self, is_end)
+
+sub _html_for_strong 	{ $_[1] ? '</strong>' 	  : '<strong>'		}
+sub _html_for_underline { $_[1] ? '</u>' 	  	  : '<u>' 			}
+sub _html_for_emphasis 	{ $_[1] ? '</em>' 	  	  : '<em>'  		}
+sub _html_for_quote		{ $_[1] ? '</blockquote>' : '<blockquote>' 	}
+sub _html_for_paragraph { $_[1] ? '</p>' 		  : '<p>'		 	}
+
 # ------------------------------------------------------------------------------
 
+sub _html_for_line_break   { '<br>'   }
+
+# ------------------------------------------------------------------------------
+
+sub _html_for_open_column  { '<div class="column">' };
+sub _html_for_close_column { '</div>' }
+
+# ------------------------------------------------------------------------------
+
+sub _html_for_open_row {
+	my ($self, $columns, $html_chunk ) = @_;
+	return qq|<div class="clearfix col-$columns">$html_chunk</div>|;	
+}
+sub _html_for_close_row { '</div>' }
+
+# ------------------------------------------------------------------------------
+
+sub _html_for_header {
+	my ($self,$level,$text) = @_;
+	return "<h$level>$text</h$level>";
+}
+
+# ------------------------------------------------------------------------------
+
+sub _html_for_link {
+	my ($self, $href, $text) = @_;	
+	return qq|<a href="$href"|
+			.($href=~/^http/?' target="_new"':'')  # External links
+			.'>'
+			.($text?$text:$href)
+			.'</a>';
+}
+
+# ------------------------------------------------------------------------------
+
+sub _html_for_image {
+	my ($self, $src, $options) = @_;
+
+	my $width  = ($options->{width}  ? ' width="'  .$options->{width}  .'px"' : '');
+	my $height = ($options->{height} ? ' height="' .$options->{height} .'px"' : '');
+
+	my $align = '';
+	if ($options->{align}) {
+		if ($options->{align} eq 'left')  {$align = ' class="pulled-left"'   }
+		if ($options->{align} eq 'right') {$align = ' class="pulled-right"'  }
+		if ($options->{align} eq 'center'){$align = ' class="centered"' 	 }
+		if ($options->{align} eq 'span')  {$align = ' class="stretch-horiz"' }
+	}
+	return  qq|<img src="$src"$align$width$height>|;
+}
+
+# ------------------------------------------------------------------------------
+
+
+
 1;
+
+__END__
+
+=head1 TITLE
+
+PML::HTMLFormatter - simple formatter for tokenized PML
+
+=head1 SYNOPSIS
+
+	use PML::HTMLFormatter;
+	
+	my $formatter = PML::HTMLFormatter->new;
+
+	my $html = $formatter->format($pml);
+
+=head1 DESCRIPTION
+
+Default HTML formatter for PML.
+
+=head1 OVERRIDABLE FORMATTER METHODS
+
+These methods should each return a chunk of html in a string.
+
+  $self->_html_for_strong( <IS_END_TAG> )
+  $self->_html_for_emphasis( <IS_END_TAG> )
+  $self->_html_for_underline( <IS_END_TAG> )
+  $self->_html_for_quote( <IS_END_TAG> )
+  $self->_html_for_header( <LEVEL>, <TEXT> )
+  $self->_html_for_link( <HREF>, <TEXT> )
+  $self->_html_for_image( <SRC>, <OPTIONS [align,width,height]> )
+
+
+=head2 _html_for_strong
+
+Return html for STRONG tags. Start and end.
+
+  @param		is_end	1=output end tag, 0=output start tag
+  @returns 	html string
+
+Examples
+
+  my $html = $self->_html_for_strong;    # '<strong>'
+  my $html = $self->_html_for_strong(1); # '</strong>'
+
+=end
+
