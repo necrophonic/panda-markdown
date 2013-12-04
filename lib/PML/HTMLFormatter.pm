5142183489a684baade7c7755aa7a1b07e426556
necrophonic <john@johngregory.me.uk>
2013-12-04 23:37:37 +0000

    Initial HTMLFormatter using PML::PullParser

--- lib/PML/HTMLFormatter.pm
+++ lib/PML/HTMLFormatter.pm
@@ -1,412 +1,163 @@
 package PML::HTMLFormatter;
 
 use v5.10;
-
 use strict;
 use warnings;
-use boolean;
 
 use Log::Log4perl qw(:easy);
-Log::Log4perl->easy_init($ERROR);
+Log::Log4perl->easy_init($TRACE);
 
 use Moo;
-use PML::Tokenizer;
+use PML::PullParser;
 
-has 'tokenizer' => ( is => 'lazy' );
 
-has 'is_paragraph_open' => ( is => 'rwp' );
-has 'is_column_open'    => ( is => 'rwp' );
-has 'is_row_open' 	    => ( is => 'rwp' );
-has 'stack'			    => ( is => 'rwp' );
 
-has 'html_chunks'	     => ( is => 'rwp' );
-has 'html_chunk_pointer' => ( is => 'rwp' );
+my %tags = (
+	STRONG_OPEN		=> '<strong>',
+	STRONG_CLOSE 	=> '</strong>',
+	EMPHASIS_OPEN	=> '<em>',
+	EMPHASIS_CLOSE 	=> '</em>',
+	UNDERLINE_OPEN	=> '<u>',
+	UNDERLINE_CLOSE => '</u>',
+	DEL_OPEN		=> '<del>',
+	DEL_CLOSE	 	=> '</del>',
 
-my $END = 1;
+	PARAGRAPH_OPEN 	=> '<p>',
+	PARAGRAPH_CLOSE	=> '</p>',
+);
 
-# ------------------------------------------------------------------------------
+
+has 'tag_stack' => (is=>'rw',default=>sub{[]});
+has 'is_paragraph_open'	=> (is=>'rw');
 
 sub format {
 	my ($self, $pml) = @_;
-	
-	$pml || $self->fatal("Must supply 'pml' to HTMLFormatter");
 
-	my @tokens 	  = @{$self->tokenizer->tokenize( $pml )->tokens};	
+	my $parser = PML::PullParser->new(pml => $pml);
 
-	$self->_set_stack([]);
-	$self->_set_is_paragraph_open(false);
-	$self->_set_is_column_open(false);
-	$self->_set_is_row_open(false);
+	my @tokens = $parser->get_all_tokens;
+	my $html   = '';
 
-
-	$self->_set_html_chunks([]);
-	$self->_set_html_chunk_pointer(0);
-
-	my $tmp_column_count = 0;
-
-	TRACE "-----------------------------------------";
-	TRACE "BEGIN HTML FORMAT";
-	
-	my $tmp_row_html = '';
-	my %tmp_row_data = ();
-
-	for my $token (@tokens) {
+	$self->is_paragraph_open(0);
 		
+	foreach my $token (@tokens) {
+
 		my $type = $token->{type};
 
-		TRACE "[Type: $type]";
+		if ($type =~ /^(STRONG|EMPHASIS|UNDERLINE|DEL)$/o) {
+			TRACE "Type [$1]";			
+			$html .= $self->_match_tag($1);
+			next;
+		}
+
+		if ($type eq 'LINK') {
+			# TODO - target
+			my $href = $token->{href};
+			my $text = $token->{text} || $token->{href};
+			$html .= qq|<a href="$href" target="_new">$text</a>|;
+			next;
+		}
+
+		if ($type eq 'IMAGE') {
+			my @options;
+			if ($token->{options}) {				
+				@options = split /,/,$token->{options};				
+			}			
+
+			my $align  = '';
+			my $height = '';
+			my $width  = '';
+
+			foreach my $option (@options) {
+				$align = ' class="pulled-left"'  if $option eq '<<';
+				$align = ' class="pulled-right"' if $option eq '>>';
+				$align = ' class="stretched"'    if $option eq '<>';
+				$align = ' class="centered"'     if $option eq '><';
+
+				if ($option =~ /^H(.+)$/) { $height = qq| height="$1px"| }
+				if ($option =~ /^W(.+)$/) { $width  = qq| width="$1px"|  }
+			}
+			
+			$html .= '<img src="'.$token->{src}.'"'.$align.$width.$height.'>';
+			next;
+		}
+
+		if ($type eq 'HEADER') {
+			$html .= "\n<h".$token->{level}.'>'.$token->{text}.'</h'.$token->{level}.">\n";
+			next;
+		}
 
 		if ($type eq 'STRING') {
-			$self->_open_paragraph_if_not_open;
-			$self->_append_to_html($token->{content});
+			$html .= $self->_open_paragraph unless $self->is_paragraph_open;
+			$html .= $token->{content};
+			next;
 		}
 
-		elsif ($type eq 'STRONG') {
-			$self->_output_simple('STRONG','strong');			
-		}
-		elsif ($type eq 'UNDERLINE') {
-			$self->_output_simple('UNDERLINE','u');			
-		}
-		elsif ($type eq 'EMPHASIS') {
-			$self->_output_simple('EMPHASIS','em');			
-		}		
-		elsif ($type eq 'QUOTE') {
-			$self->_output_simple('QUOTE','blockquote');			
-		}		
+		# Shouldn't get here!
+		# TODO error
 
-		elsif ($type eq 'PARA') {
-			if ($self->is_paragraph_open) {
-				unless ($self->_is_tag_matched('PARA')) {
-					$self->fatal("Expecting to close paragraph but bad stack head");
-				}
-				$self->_pop_from_stack;				
-				$self->_append_to_html( $self->_html_for_paragraph($END) );
-				$self->_set_is_paragraph_open(false);
-			}
-			else {
-				$self->_append_to_html( $self->_html_for_paragraph );
-				$self->_set_is_paragraph_open(true);
-			}
-		}
-
-		elsif ($type eq 'LINE_BREAK') {
-			$self->_open_paragraph_if_not_open;			
-			$self->_append_to_html( $self->_html_for_line_break );
-		}
-
-		# HEADER
-		elsif ($type eq 'HEADER') {			
-			my $level = $token->{level};
-			my $text  = $token->{text};			
-			$self->_append_to_html( $self->_html_for_header( $level, $text ));
-		}
-
-		# ROW
-		elsif ($type eq 'ROW') {			
-
-			if ($self->is_row_open) {
-				# If a column is still open, then close it
-				if ($self->is_column_open) {					
-					$self->_close_paragraph_if_open;
-					$self->_append_to_html( $self->_html_for_close_row );
-					$self->_set_is_column_open(false);	
-				}
-
-				# If there's already a row open, then this must close it.
-				$self->_set_is_row_open(false);
-				$self->_close_paragraph_if_open;
-
-				$self->html_chunks->[ $self->html_chunk_pointer ]
-							= $self->_html_for_open_row(
-										$tmp_column_count,
-										$self->html_chunks->[ $self->html_chunk_pointer ]
-							);
-
-				$tmp_column_count = 0; # Reset count
-			}
-			else {
-				# Not already a row open, so open one
-				%tmp_row_data = (rows=>[]);
-				$self->_set_is_row_open(true);			
-			}
-			
-			# Start or move to a new html chunk
-			$self->_inc_chunk_pointer;
-		}
-
-		# COLUMN
-		elsif ($type eq 'COLUMN') {			
-
-			$self->fatal("Column outside of row") unless $self->is_row_open;
-			
-			if ($self->is_column_open) {
-				TRACE "Column is open - closing";
-				$self->_close_paragraph_if_open;
-				$self->_append_to_html( $self->_html_for_close_column );				
-			}
-			
-			# If a previous paragraph was open, then we want to
-			# close it before we start a new column.
-			$tmp_column_count++;			
-			$self->_append_to_html( $self->_html_for_open_column );
-			$self->_set_is_column_open(true);
-		}
-
-		# IMAGE
-		elsif ($type eq 'IMAGE') {
-			# If the image is inline (no specified alignment) then
-			# open a paragraph if one isn't open.			
-			$self->_open_paragraph_if_not_open unless $token->{align};
-			$self->_append_to_html( $self->_html_for_image(
-										$token->{src},
-										{
-											align  => $token->{align},
-											width  => $token->{width},
-											height => $token->{height},
-											align  => $token->{align},
-										}  
-			 						));
-		}
-
-		# LINK
-		elsif ($type eq 'LINK') {
-			$self->_open_paragraph_if_not_open;
-			$self->_append_to_html( $self->_html_for_link( $token->{href}, $token->{text}) );
-		}
 	}
 
-	# Clean up
-	# Clear down stack
-	for my $tag (@{$self->stack}) {
-		if ($tag eq 'PARA') { $self->_append_to_html( $self->_html_for_paragraph($END)) }
-	}
+	# If there's a paragraph open, close it!
+	$html .= $tags{PARAGRAPH_CLOSE} if $self->is_paragraph_open;
 
-	return join '', @{$self->html_chunks};
+	return $html;
 }
 
 # ------------------------------------------------------------------------------
 
-sub _append_to_html {
-	my ($self, $text_to_append) = @_;	
-	TRACE "Append '$text_to_append' to html chunk '".$self->html_chunk_pointer."'";
-	$self->html_chunks->[$self->html_chunk_pointer] .= $text_to_append;	
-}
+sub _match_tag {
+	my ($self, $type) = @_;
 
-# ------------------------------------------------------------------------------
-
-sub _inc_chunk_pointer {
-	my ($self) = @_;
-
-	# Increment if there's content in the current chunk, otherwise carry on using it
-	if ($self->html_chunks->[$self->html_chunk_pointer]) {
-		$self->_set_html_chunk_pointer( $self->html_chunk_pointer + 1 );
+	if (@{$self->tag_stack} && $self->tag_stack->[0] eq $type) {		
+		# Close tag
+		$self->_pop_stack;
+		return $tags{$type."_CLOSE"};
+	}
+	else {		
+		# Open tag
+		my $html = '';		
+		# If a paragraph isn't open then we need to open one!
+		$html = $self->_open_paragraph unless $self->is_paragraph_open;		
+		$self->_push_stack($type);
+		return $html . $tags{$type."_OPEN"};
 	}
 	return;
 }
 
 # ------------------------------------------------------------------------------
 
-sub _open_paragraph_if_not_open {
+sub _push_stack {
+	my ($self, $type) = @_;
+	unshift @{$self->tag_stack}, $type;
+}
+
+# ------------------------------------------------------------------------------
+
+sub _pop_stack {
 	my ($self) = @_;
-	unless ($self->is_paragraph_open) {		
-		$self->_append_to_html( $self->_html_for_paragraph );
-		$self->_set_is_paragraph_open(true);
-		$self->_add_to_stack('PARA');
-		TRACE "Close paragaph";
-	}
+	return shift @{$self->tag_stack};
 }
 
 # ------------------------------------------------------------------------------
 
-sub _close_paragraph_if_open {
+sub _open_paragraph {
 	my ($self) = @_;
-	if ($self->is_paragraph_open) {
-		$self->_append_to_html( $self->_html_for_paragraph($END));
-		$self->_set_is_paragraph_open(false);
-		my $tag = $self->_pop_from_stack;
-		if ($tag ne 'PARA') {
-			$self->fatal("_close_paragraph_if_open expected PARA at end of stack");
-		}
-	}
+	die "Can't open paragraph - already open!" if $self->is_paragraph_open;
+	$self->_push_stack('PARAGRAPH');
+	$self->is_paragraph_open(1);
+	return $tags{PARAGRAPH_OPEN};
 }
 
 # ------------------------------------------------------------------------------
 
-sub _output_simple {
-	my ($self, $tag) = @_;
-	my $match = $self->_is_tag_matched($tag);
-	if ($match) {
-		shift @{$self->stack}; # Pull match off stack
-		$self->_append_to_html( $self->_html_for_strong($END) ) 	if $tag eq 'STRONG';
-		$self->_append_to_html( $self->_html_for_underline($END) ) 	if $tag eq 'UNDERLINE';
-		$self->_append_to_html( $self->_html_for_emphasis($END) ) 	if $tag eq 'EMPHASIS';
-		$self->_append_to_html( $self->_html_for_quote($END) ) 		if $tag eq 'QUOTE';
-	}
-	else {
-		# Check whether paragaph is open and open if not.
-		unless ($self->is_paragraph_open) {
-			$self->_append_to_html( $self->_html_for_paragraph );
-			unshift @{$self->stack}, 'PARA';
-			$self->_set_is_paragraph_open(true);
-		}		
-		$self->_append_to_html( $self->_html_for_strong ) 	 if $tag eq 'STRONG';
-		$self->_append_to_html( $self->_html_for_underline ) if $tag eq 'UNDERLINE';
-		$self->_append_to_html( $self->_html_for_emphasis )  if $tag eq 'EMPHASIS';
-		$self->_append_to_html( $self->_html_for_quote )     if $tag eq 'QUOTE';
-		unshift @{$self->stack}, $tag;
-	}
-}
-
-# ------------------------------------------------------------------------------
-sub _add_to_stack {
-	my ($self, $tag) = @_;
-	TRACE "Add '$tag' to stack";
-	unshift @{$self->stack}, $tag;
-}
-
-# ------------------------------------------------------------------------------
-
-sub _pop_from_stack {
-	my ($self, $stack_ref) = @_;
-	my $tag = shift @{$self->stack};
-	TRACE "Pop '$tag' from stack";
-	return $tag;
-}
-
-# ------------------------------------------------------------------------------
-
-sub _is_tag_matched {
-	my ($self, $tag) = @_;
-	return @{$self->stack} && $self->stack->[0] eq $tag ? true : false;
-}
-
-# ------------------------------------------------------------------------------
-
-sub fatal {
-	my ($self, @msg) = @_;
-	my $msg = join '',@msg;
-	ERROR "!!HTMLFormatter Error: $msg!!";
-	die "$msg\n\n";
-}
-
-# --------------------------------------------------------------[ BUILDERS ]----
-
-sub _build_tokenizer {
+sub _close_paragraph {
 	my ($self) = @_;
-	return PML::Tokenizer->new;
+	die "Can't close paragraph - already closed!" unless $self->is_paragraph_open;
+	die "Can't close paragraph - bad stack match" unless $self->tag_stack->[0] eq 'PARAGRAPH';
+	$self->_pop_stack;
+	$self->is_paragraph_open(0);
+	return $tags{PARAGRAPH_CLOSE};
 }
-
-# --------------------------------------------------------[ HTML OUTPUTERS ]----
-
-# <sub>(self, is_end)
-
-sub _html_for_strong 	{ $_[1] ? '</strong>' 	  : '<strong>'		}
-sub _html_for_underline { $_[1] ? '</u>' 	  	  : '<u>' 			}
-sub _html_for_emphasis 	{ $_[1] ? '</em>' 	  	  : '<em>'  		}
-sub _html_for_quote		{ $_[1] ? '</blockquote>' : '<blockquote>' 	}
-sub _html_for_paragraph { $_[1] ? '</p>' 		  : '<p>'		 	}
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_line_break   { '<br>'   }
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_open_column  { '<div class="column">' };
-sub _html_for_close_column { '</div>' }
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_open_row {
-	my ($self, $columns, $html_chunk ) = @_;
-	return qq|<div class="clearfix col-$columns">$html_chunk</div>|;	
-}
-sub _html_for_close_row { '</div>' }
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_header {
-	my ($self,$level,$text) = @_;
-	return "<h$level>$text</h$level>";
-}
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_link {
-	my ($self, $href, $text) = @_;	
-	return qq|<a href="$href"|
-			.($href=~/^http/?' target="_new"':'')  # External links
-			.'>'
-			.($text?$text:$href)
-			.'</a>';
-}
-
-# ------------------------------------------------------------------------------
-
-sub _html_for_image {
-	my ($self, $src, $options) = @_;
-
-	my $width  = ($options->{width}  ? ' width="'  .$options->{width}  .'px"' : '');
-	my $height = ($options->{height} ? ' height="' .$options->{height} .'px"' : '');
-
-	my $align = '';
-	if ($options->{align}) {
-		if ($options->{align} eq 'left')  {$align = ' class="pulled-left"'   }
-		if ($options->{align} eq 'right') {$align = ' class="pulled-right"'  }
-		if ($options->{align} eq 'center'){$align = ' class="centered"' 	 }
-		if ($options->{align} eq 'span')  {$align = ' class="stretch-horiz"' }
-	}
-	return  qq|<img src="$src"$align$width$height>|;
-}
-
-# ------------------------------------------------------------------------------
-
-
 
 1;
-
-__END__
-
-=head1 TITLE
-
-PML::HTMLFormatter - simple formatter for tokenized PML
-
-=head1 SYNOPSIS
-
-	use PML::HTMLFormatter;
-	
-	my $formatter = PML::HTMLFormatter->new;
-
-	my $html = $formatter->format($pml);
-
-=head1 DESCRIPTION
-
-Default HTML formatter for PML.
-
-=head1 OVERRIDABLE FORMATTER METHODS
-
-These methods should each return a chunk of html in a string.
-
-  $self->_html_for_strong( <IS_END_TAG> )
-  $self->_html_for_emphasis( <IS_END_TAG> )
-  $self->_html_for_underline( <IS_END_TAG> )
-  $self->_html_for_quote( <IS_END_TAG> )
-  $self->_html_for_header( <LEVEL>, <TEXT> )
-  $self->_html_for_link( <HREF>, <TEXT> )
-  $self->_html_for_image( <SRC>, <OPTIONS [align,width,height]> )
-
-
-=head2 _html_for_strong
-
-Return html for STRONG tags. Start and end.
-
-  @param		is_end	1=output end tag, 0=output start tag
-  @returns 	html string
-
-Examples
-
-  my $html = $self->_html_for_strong;    # '<strong>'
-  my $html = $self->_html_for_strong(1); # '</strong>'
-
-=end
-
--- t/html_formatter.t
+++ t/html_formatter.t
@@ -1,132 +1,172 @@
 #!/usr/bin/env perl
+
 use strict;
 use Test::More;
-use Test::Exception;
 
 use_ok 'PML::HTMLFormatter';
 
-{
-	my $formatter = new_ok 'PML::HTMLFormatter';
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($OFF);
 
-	isa_ok($formatter->tokenizer,'PML::Tokenizer');
+	can_ok('PML::HTMLFormatter',qw|format _match_tag|);
 
-	dies_ok { $formatter->format } "Dies when no pml supplied to format()";
-}
+	test_simple();
+	test_headers();
+	test_links();
+	test_images();
+#	test_full_doc_1();
 
-subtest "Simple text to paragaph" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	my $html = $formatter->format('Some basic text!');
-	is($html,'<p>Some basic text!</p>','Plain text');
-};
-
-
-subtest "Simple style formatting" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format('**strong**, __underline__, //emphasis//, ""quote""')
-	  ,q|<p><strong>strong</strong>, <u>underline</u>, <em>emphasis</em>, <blockquote>quote</blockquote></p>|
-	  ,'PML with strong, underline and emphasis');
-};
-
-subtest "Plain control chars" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format('*a"a/a"a_a')
-	  ,q|<p>*a"a/a"a_a</p>|
-	  ,'Plain chars output as string text');
-};
-
-
-subtest "Paragraphs" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format("Paragraph then\n\nNew paragaph")
-	  ,q|<p>Paragraph then</p><p>New paragaph</p>|
-	  ,'Paragraph break');
-
-	is($formatter->format("Paragraph then\nline break")
-	  ,q|<p>Paragraph then<br>line break</p>|
-	  ,'Newline break');
-};
-
-
-subtest "Links" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format("[[http://google.com]]")
-	  ,q|<p><a href="http://google.com" target="_new">http://google.com</a></p>|
-	  ,'Simple external link');
-
-	is($formatter->format("[[/assets/something]]")
-	  ,q|<p><a href="/assets/something">/assets/something</a></p>|
-	  ,'Simple internal link');
-
-	is($formatter->format("[[http://google.com|Google]]")
-	  ,q|<p><a href="http://google.com" target="_new">Google</a></p>|
-	  ,'Simple external link with alt text');
-
-	is($formatter->format("Text [[a]] Other Text")
-	  ,q|<p>Text <a href="a">a</a> Other Text</p>|
-	  ,'Link in existing paragaph');
-};
-
-
-subtest "Images" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format('{{http://a.com/abc.png}}')
-	  ,q|<p><img src="http://a.com/abc.png"></p>|
-	  ,"Absolute image with no options");
-
-	is($formatter->format('{{abc.png}}')
-	  ,q|<p><img src="abc.png"></p>|
-	  ,"Relative image with no options");
-
-	is($formatter->format('{{abc.png|<<}}')
-	  ,q|<img src="abc.png" class="pulled-left">|
-	  ,"Image with align left");
-
-	is($formatter->format('{{abc.png|>>}}')
-	  ,q|<img src="abc.png" class="pulled-right">|
-	  ,"Image with align right");
-
-	is($formatter->format('{{abc.png|<>}}')
-	  ,q|<img src="abc.png" class="stretch-horiz">|
-	  ,"Image with stretch horizontal");
-
-	is($formatter->format('{{abc.png|><}}')
-	  ,q|<img src="abc.png" class="centered">|
-	  ,"Image centered");
-
-	is($formatter->format('{{abc.png|W10,H20}}')
-	  ,q|<p><img src="abc.png" width="10px" height="20px"></p>|
-	  ,"Image with dimensions");
-};
-
-
-subtest "Headers" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format('##1|Blah##')
-	  ,q|<h1>Blah</h1>|
-	  ,"Independant header (level 1)");
-
-	is($formatter->format('##2|Blah##')
-	  ,q|<h2>Blah</h2>|
-	  ,"Independant header (level 2)");
-
-	is($formatter->format('##3|Blah##')
-	  ,q|<h3>Blah</h3>|
-	  ,"Independant header (level 3)");
-
-	subtest "Bad header sequence" => sub {
-		is($formatter->format('#39')
-	  	  ,q|<p>#39</p>|
-	  	  ,"Single hash becomes string");	
-	};
-};
-
-
-subtest "Rows and columns" => sub {
-	my $formatter = PML::HTMLFormatter->new;
-	is($formatter->format('@@||a||b@@')
-	  ,q|<div class="clearfix col-2"><div class="column"><p>a</p></div><div class="column"><p>b</p></div></div>|
-	  ,"2-col row");
-};
-
+	# TESTS TO DO
+	# .. html escape
+	# .. entity escape
 
 done_testing();
+
+
+# ==============================================================================
+
+sub test_simple {
+	subtest "Simple markup test" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('**abc**'),'<p><strong>abc</strong></p>','Strong');
+		is($parser->format('//abc//'),'<p><em>abc</em></p>','Emphasis');
+		is($parser->format('__abc__'),'<p><u>abc</u></p>','Underline');
+		is($parser->format('--abc--'),'<p><del>abc</del></p>','Delete');
+	};
+}
+
+# ------------------------------------------------------------------------------
+
+sub test_headers {
+	subtest "Simple markup test" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('# My header'),	"\n<h1>My header</h1>\n",'Header 1');
+		is($parser->format('## My header'),	"\n<h2>My header</h2>\n",'Header 2');
+		is($parser->format('### My header'),"\n<h3>My header</h3>\n",'Header 3');
+	};
+}
+
+# ------------------------------------------------------------------------------
+
+sub test_links {
+	subtest "Simple links" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('[[http://here.com]]'),
+			'<a href="http://here.com" target="_new">http://here.com</a>',
+			'Simple link - no text');
+
+		is($parser->format('[[http://here.com|HERE]]'),
+			'<a href="http://here.com" target="_new">HERE</a>',
+			'Simple link - with text');
+	};
+}
+
+# ------------------------------------------------------------------------------
+
+sub test_images {
+	subtest "Simple images" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('{{image.jpg}}'), '<img src="image.jpg">', 'Relative image');
+		is($parser->format('{{http://a.com/image.jpg}}'), '<img src="http://a.com/image.jpg">', 'Absolute image');
+	};
+
+	subtest "Images with align options" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('{{i.jpg|<<}}'), '<img src="i.jpg" class="pulled-left">',  'Pull left');
+		is($parser->format('{{i.jpg|>>}}'), '<img src="i.jpg" class="pulled-right">', 'Pull right');
+		is($parser->format('{{i.jpg|><}}'), '<img src="i.jpg" class="centered">', 	  'Centered');
+		is($parser->format('{{i.jpg|<>}}'), '<img src="i.jpg" class="stretched">', 	  'Stretched');
+	};
+
+	subtest "Images with width options" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('{{i.jpg|W10}}'), '<img src="i.jpg" width="10px">', 'Width 10');
+		is($parser->format('{{i.jpg|W9}}'),  '<img src="i.jpg" width="9px">',  'Width 9');		
+	};
+
+	subtest "Images with height options" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('{{i.jpg|H10}}'), '<img src="i.jpg" height="10px">', 'Height 10');
+		is($parser->format('{{i.jpg|H9}}'),  '<img src="i.jpg" height="9px">',  'Height 9');		
+	};
+
+	subtest "Images with mixed options" => sub {
+		my $parser = PML::HTMLFormatter->new;
+		is($parser->format('{{i.jpg|<<,W10,H11}}'),
+			'<img src="i.jpg" class="pulled-left" width="10px" height="11px">',
+			'All options');
+	};
+}
+
+# ------------------------------------------------------------------------------
+
+sub test_full_doc_1 {
+	subtest "Full doc #1" => sub {
+
+		my $input_pml = <<EOT
+Yup, we're here! **Caffeinated Panda Creations** has launched the first phase of our new website.
+
+Right now as you can see the blog is up and running and we'll be using it to keep you up to date with projects, events we're involved with, and new features coming to the site. Of course we'll still be streaming updates and content on [[http://facebook.com/caffeinatedpandacreations|facebook]] and [[http://twitter.com/cafpanda|twitter]] as well so come and join us there too!
+
+As time goes on we'll be adding new sections to the website so stay tuned for updates. Upcoming soon will be more details on our creations and services, so here's some things to whet your appetite!
+==
+||
+## Custom Cyberfalls
+{{cyberfalls.jpg|<<,H100,W130}}Cyberfalls for all your cybergoth and dance needs, at Caffeinated Panda Creations we specialise in custom designs and sets with [[https://www.google.co.uk/search?q=el+wire&safe=off&tbm=isch|EL-wire]] installations.
+
+Whether you're looking for a themed set for a special occassion or straight forward cyber-chic, get in touch and we'll be happy to work with you!
+||
+## 3D Printing
+{{makerbot.jpg|<<,H100,W130}}We here at Caffeinated Panda Creations are the proud owners of a [[http://store.makerbot.com/replicator2.html|Makerbot Replicator 2]] 3D printer.
+
+As well as using it for our creations we can also offer a bespoke printing service. Design your own objects or let us work with you to create and realise your vision in high quality PLA plastic.
+==
+
+==
+||
+## Emporia
+{{etsy.jpg|<<,H100,W130}}Over the next few months we'll be opening our online stores where we'll be selling some of our premade and customisable pieces along with t-shirts and other apparel.
+
+||
+## Costuming
+{{mask.jpg|<<,H100,W130}}The panda team are keen costumers and going forward will be working on several exciting cosplaying projects for ourselves and others.
+
+We'll also be keeping you up to date on where we'll be appearing and giving details on how we can work with you on your own costuming projects.
+==
+EOT
+;
+
+		my $expected_html =<<EOT
+<p>Yup, we're here! <strong>Caffeinated Panda Creations</strong> has launched the first phase of our new website.</p><p>Right now as you can see the blog is up and running and we'll be using it to keep you up to date with projects, events we're involved with, and new features coming to the site. Of course we'll still be streaming updates and content on <a href="http://facebook.com/caffeinatedpandacreations" target="_new">facebook</a> and <a href="http://twitter.com/cafpanda" target="_new">twitter</a> as well so come and join us there too!</p><p>As time goes on we'll be adding new sections to the website so stay tuned for updates. Upcoming soon will be more details on our creations and services, so here's some things to whet your appetite!
+<div class="clearfix col-2">
+<div class="column">
+<h2>Custom Cyberfalls</h2>
+<img src="/content/blog/the-panda-cometh/cyberfalls.jpg" class="pulled-left" width="130px" height="100px">Cyberfalls for all your cybergoth and dance needs, at Caffeinated Panda Creations we specialise in custom designs and sets with <a href="https://www.google.co.uk/search?q=el+wire&amp;safe=off&amp;tbm=isch" target="_new">EL-wire</a> installations.</p><p>Whether you're looking for a themed set for a special occassion or straight forward cyber-chic, get in touch and we'll be happy to work with you!</p>
+</div>
+<div class="column">
+<h2>3D Printing</h2>
+<img src="/content/blog/the-panda-cometh/makerbot.jpg" class="pulled-left" width="130px" height="100px"><p>We here at Caffeinated Panda Creations are the proud owners of a <a href="http://store.makerbot.com/replicator2.html" target="_new">Makerbot Replicator 2</a> 3D printer.</p><p>As well as using it for our creations we can also offer a bespoke printing service. Design your own objects or let us work with you to create and realise your vision in high quality PLA plastic.</p>
+</div>
+</div>
+<div class="clearfix col-2">
+<div class="column">
+<h2>Emporia</h2>
+<img src="/content/blog/the-panda-cometh/etsy.jpg" class="pulled-left" width="130px" height="100px"><p>Over the next few months we'll be opening our online stores where we'll be selling some of our premade and customisable pieces along with t-shirts and other apparel.</p>
+</div>
+<div class="column">
+<h2>Costuming</h2>
+<img src="/content/blog/the-panda-cometh/mask.jpg" class="pulled-left" width="130px" height="100px"><p>The panda team are keen costumers and going forward will be working on several exciting cosplaying projects for ourselves and others.</p><p>We'll also be keeping you up to date on where we'll be appearing and giving details on how we can work with you on your own costuming projects.</p>
+</div>
+</div>
+EOT
+;
+
+		my $formatter = PML::HTMLFormatter->new();
+		my $html = $formatter->format( $input_pml );
+		is( $html, $expected_html, 'HTML as expected' );
+
+	};
+	return;
+}
+
+
